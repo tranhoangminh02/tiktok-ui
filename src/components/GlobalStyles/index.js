45e29e2f59a00025c7e24a765ceba43b24824294
Minh Hoàng <tranhoangminh.958@gmail.com>
2022-12-12 10:59:33 +0700

    tiktok-ui

---
+++ src/components/GlobalStyles/index.js
@@ -0,0 +1,7 @@
+import './GlobalStyles.scss';
+import 'normalize.css';
+
+function GlobalStyles({ children }) {
+    return children;
+}
+export default GlobalStyles;
---
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -0,0 +1,5 @@
+function Header() {
+    return <h2>Header pages</h2>;
+}
+
+export default Header;
